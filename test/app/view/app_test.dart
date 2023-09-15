b79d5d68cfc776bd8fec44fd6f9a2fc488db201d
luzemma <luzemma.gonzalez@gmail.com>
2023-09-15 09:19:26 -0600

    removes useless counter page

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:marimba_app/app/app.dart';
-import 'package:marimba_app/counter/counter.dart';
+import 'package:marimba_app/xylophone/xylophone_screen.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders XylophoneScreen', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(XylophoneScreen), findsOneWidget);
     });
   });
 }
