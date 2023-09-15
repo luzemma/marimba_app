9d4822cc3c4280bf89fbbd96d86eac944f2558ab
Luzemma Gonzalez <luzemmagonzalez@MacBook-Pro-de-Luzemma.local>
2023-09-14 18:06:58 -0600

    adding animation controller to each item of xylophone

--- lib/app/widgets/xylophone.dart
+++ lib/app/widgets/xylophone.dart
@@ -25,47 +25,40 @@ class _XylophoneState extends State<Xylophone> {
       children: [
         XylophoneButton(
           color: Colors.red,
-          number: 1,
-          //onTap: () => playSound(1),
+          playSound: () => playSound(1),
         ),
         XylophoneButton(
           color: Colors.orange,
-          number: 2,
-          //onTap: () => playSound(2),
+          playSound: () => playSound(2),
         ),
         XylophoneButton(
           color: Colors.yellow,
-          number: 3,
-          //onTap: () => playSound(3),
+          playSound: () => playSound(3),
         ),
         XylophoneButton(
           color: Colors.green,
-          number: 4,
-          //onTap: () => playSound(3),
+          playSound: () => playSound(3),
         ),
         XylophoneButton(
           color: Colors.teal,
-          number: 5,
-          //onTap: () => playSound(5),
+          playSound: () => playSound(5),
         ),
         XylophoneButton(
           color: Colors.blue,
-          number: 6,
-          //onTap: () => playSound(6),
+          playSound: () => playSound(6),
         ),
         XylophoneButton(
           color: Colors.purple,
-          number: 7,
-          //onTap: () => playSound(7),
+          playSound: () => playSound(7),
         ),
       ],
     );
   }
 
-  void playSound(int number) async {
+  Future<void> playSound(int number) async {
     await _audioPlayer.stop();
-    _audioPlayer.play(
-      AssetSource('assets/audios/note$number.wav'),
+    await _audioPlayer.play(
+      AssetSource('audios/note$number.wav'),
     );
   }
 
--- lib/app/widgets/xylophone_button.dart
+++ lib/app/widgets/xylophone_button.dart
@@ -1,40 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:marimba_app/app/widgets/oval_painter.dart';
 
-class XylophoneButton extends StatelessWidget {
+class XylophoneButton extends StatefulWidget {
   const XylophoneButton({
     required this.color,
-    required this.number,
+    required this.playSound,
     super.key,
   });
 
   final Color color;
-  final int number;
+  final void Function() playSound;
+
+  @override
+  State<XylophoneButton> createState() => _XylophoneButtonState();
+}
+
+class _XylophoneButtonState extends State<XylophoneButton>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> _animation;
+  late AnimationController _controller;
+
+  double scale = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    final scaleTween = Tween<double>(begin: 1, end: 0.95);
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+    _animation = scaleTween.animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.elasticOut,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: const EdgeInsets.only(bottom: 8),
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            CustomPaint(
-              painter: OvalPainter(backgroundColor: Colors.black),
-            ),
-            const SizedBox(
-              width: 100,
-            ),
-            CustomPaint(
-              painter: OvalPainter(backgroundColor: Colors.black),
-            )
-          ],
+    return GestureDetector(
+      onTap: () {
+        _animate();
+        widget.playSound.call();
+      },
+      child: ScaleTransition(
+        scale: _animation,
+        child: Container(
+          height: 80,
+          margin: const EdgeInsets.only(bottom: 8),
+          decoration: BoxDecoration(
+            color: widget.color,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CustomPaint(
+                painter: OvalPainter(backgroundColor: Colors.black),
+              ),
+              const SizedBox(
+                width: 100,
+              ),
+              CustomPaint(
+                painter: OvalPainter(backgroundColor: Colors.black),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _animate() {
+    _controller
+      ..reset()
+      ..forward(from: 0)
+      ..reverse(from: 1);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
